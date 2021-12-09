22915b9250b2700be50ab6af2795db28f4779940
Hugo Mota <hugo.mota@ambisig.com>
2021-12-09 11:20:32 -0300

    Initial Config

---
+++ src/main/java/com/hugo/mota/projectmanagementsecurityaccesscontrol/ProjectManagementSecurityAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.mota.projectmanagementsecurityaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSecurityAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementSecurityAccessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/mota/projectmanagementsecurityaccesscontrol/ProjectManagementSecurityAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.mota.projectmanagementsecurityaccesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSecurityAccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
